4879926199576f10de2fde0d50ec03462be128d7
Micah Sterling <msterling89@gmail.com>
2022-12-15 16:23:39 -0600

    update axios

--- src/axios.js
+++ src/axios.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://frontend-take-home.fetchrewards.com"
-      : "http://localhost:3000",
-  headers: { "Content-Type": "application/json" },
-  withCredentials: true,
+    "https://frontend-take-home.fetchrewards.com",
+  // process.env.NODE_ENV === "production"
+  //   ? "https://frontend-take-home.fetchrewards.com"
+  //   : "http://localhost:3000",
+  // headers: { "Content-Type": "application/json" },
+  // withCredentials: true,
 });
