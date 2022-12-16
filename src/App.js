497761d9a2b54634ce50b1ed8a2cdbc2043fbfba
Micah Sterling <msterling89@gmail.com>
2022-12-15 18:41:38 -0600

    add occupation

--- src/App.js
+++ src/App.js
@@ -1,37 +1,98 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "./axios";
 
 function App() {
   const [formData, setFormData] = useState({});
+  const [data, setData] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  const handleFormChange = (e) => {
+    e.preventDefault();
+
+    setFormData(
+      Object.assign({}, formData, { [e.target.name]: e.target.value })
+    );
+  };
+
+  useEffect(() => {
+    axios
+      .get("/form")
+      .then((resp) => {
+        setData(resp.data);
+        setLoaded(true);
+      })
+      .catch((resp) => console.log(resp));
+  }, []);
+
+  console.log("state", formData);
+
+  const occupations = data.occupations;
+  const state = data.states;
 
   const handleSubmit = () => { };
   return (
     <div className="container">
-
       <div className="wrapper">
-        <form className="form" onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <span className="form-title">Registration Form</span>
-          <div className="wrap-input validate-input">
-            <input className="form-input" type="text" placeholder="Full Name" />
+          <div className="input-wrap validate-input">
+            <input
+              className="form-input"
+              type="text"
+              name="fullName"
+              value={formData.fullName}
+              placeholder="Full Name"
+              onChange={handleFormChange}
+            />
           </div>
-          <div className="wrap-input validate-input">
-            <input className="form-input" type="email" placeholder="Email" />
+          <div className="input-wrap validate-input">
+            <input
+              className="form-input"
+              type="email"
+              name="email"
+              value={formData.email}
+              placeholder="Email"
+              onChange={handleFormChange}
+            />
           </div>
-          <div className="wrap-input validate-input">
-            <input className="form-input" type="password" placeholder="Password" />
+          <div className="input-wrap form- validate-input">
+            <input
+              className="form-input"
+              type="password"
+              name="password"
+              value={formData.password}
+              placeholder="Password"
+              onChange={handleFormChange}
+            />
           </div>
-          <div className="wrap-input validate-input">
-            <input className="form-input" type="text" placeholder="Occupation" />
-          </div>
-          <div className="wrap-input validate-input">
-            <input className="form-input" type="text" placeholder="State" />
+          <select
+            className="input-wrap validate-input"
+            name="occupation"
+            onChange={handleFormChange}
+            required
+          >
+            <option value="" disabled hidden selected>
+              Select an Occupation
+            </option>
+            {loaded &&
+              occupations.map((item) => {
+                return <option>{item}</option>;
+              })}
+          </select>
+          <div className="input-wrap validate-input">
+            <input
+              className="form-input"
+              type="text"
+              name="state"
+              value={formData.state}
+              placeholder="State"
+              onChange={handleFormChange}
+            />
           </div>
           <div className="container-form-btn">
             <button className="form-btn">
-              <span>
-                Send
-              </span>
+              <span>Send</span>
             </button>
           </div>
         </form>
--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:
-    "https://frontend-take-home.fetchrewards.com",
-  // process.env.NODE_ENV === "production"
-  //   ? "https://frontend-take-home.fetchrewards.com"
-  //   : "http://localhost:3000",
-  // headers: { "Content-Type": "application/json" },
-  // withCredentials: true,
+  baseURL: "https://frontend-take-home.fetchrewards.com",
 });
