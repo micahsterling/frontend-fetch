dfb9ded231a03f5e7e2c036be8f4591d66b744f6
Micah Sterling <msterling89@gmail.com>
2022-12-16 15:41:16 -0600

    add successful form submission

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [formData, setFormData] = useState({});
   const [data, setData] = useState([]);
   const [loaded, setLoaded] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
   const handleFormChange = (e) => {
     e.preventDefault();
@@ -30,83 +31,113 @@ function App() {
   const occupations = data.occupations;
   const state = data.states;
 
-  const handleSubmit = () => { };
-  return (
-    <div className="container">
-      <div className="wrapper">
-        <form className="validate-form" onSubmit={handleSubmit}>
-          <span className="form-title">Registration Form</span>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("/form", formData)
+      .then((resp) => {
+        setSubmitted(true);
+      })
+      .catch((resp) => console.log("catch", resp));
+    setFormData("");
+  };
 
-          <div className="input-wrap">
-            <input
-              className="form-input"
-              type="text"
-              name="fullName"
-              value={formData.fullName}
-              placeholder="Full Name"
-              onChange={handleFormChange}
-              required
-            />
-          </div>
-          <div className="input-wrap">
-            <input
-              className="form-input"
-              type="email"
-              name="email"
-              value={formData.email}
-              placeholder="Email"
-              onChange={handleFormChange}
-              required
-            />
-          </div>
-          <div className="input-wrap">
-            <input
-              className="form-input"
-              type="password"
-              name="password"
-              value={formData.password}
-              placeholder="Password"
-              onChange={handleFormChange}
-              required
-            />
-          </div>
-          <select
-            className="input-wrap"
-            name="occupation"
-            onChange={handleFormChange}
-            required
-          >
-            <option value="" disabled hidden selected>
-              Select an Occupation
-            </option>
-            {loaded &&
-              occupations.map((item) => {
-                return <option key={item}> {item}</option>;
-              })}
-          </select>
-          <select
-            className="input-wrap"
-            name="state"
-            onChange={handleFormChange}
-            required
-          >
-            <option value="" disabled hidden selected>
-              Select the state you live in
-            </option>
-            {loaded &&
-              state.map((item) => {
-                return <option key={item.name}>{item.name}</option>;
-              })}
-          </select>
-          <div className="container-form-btn">
-            <button className="form-btn">
-              <span>Send</span>
+  const handleClick = () => {
+    setSubmitted(false);
+  };
+
+  if (submitted) {
+    return (
+      <div className="container success">
+        <div className="success-wrap">
+          <span className="success-title">Your Form has been Submitted</span>
+          <div className="return-btn">
+            <button className="form-btn" onClick={handleClick}>
+              Make Another Entry
             </button>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="container">
+        <div className="wrapper">
+          <form onSubmit={handleSubmit}>
+            <span className="form-title">Registration Form</span>
+
+            <div className="input-wrap">
+              <input
+                className="form-input"
+                type="text"
+                name="name"
+                value={formData.fullName}
+                placeholder="Name"
+                onChange={handleFormChange}
+                required
+              />
+              <span className="focus-input"></span>
+            </div>
+            <div className="input-wrap">
+              <input
+                className="form-input"
+                type="email"
+                name="email"
+                value={formData.email}
+                placeholder="Email"
+                onChange={handleFormChange}
+                required
+              />
+            </div>
+            <div className="input-wrap">
+              <input
+                className="form-input"
+                type="password"
+                name="password"
+                value={formData.password}
+                placeholder="Password"
+                onChange={handleFormChange}
+                required
+              />
+            </div>
+            <select
+              className="input-wrap"
+              name="occupation"
+              onChange={handleFormChange}
+              required
+            >
+              <option value="" disabled hidden selected>
+                Select an Occupation
+              </option>
+              {loaded &&
+                occupations.map((item) => {
+                  return <option key={item}> {item}</option>;
+                })}
+            </select>
+            <select
+              className="input-wrap"
+              name="state"
+              onChange={handleFormChange}
+              required
+            >
+              <option value="" disabled hidden selected>
+                Select the state you live in
+              </option>
+              {loaded &&
+                state.map((item) => {
+                  return <option key={item.name}>{item.name}</option>;
+                })}
+            </select>
+            <div className="container-form-btn">
+              <button className="form-btn">
+                <span>Send</span>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
